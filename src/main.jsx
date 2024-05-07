d6898b9d9b102426e6b776e28851dac6b31760d2
jhosuapp <jhosuapvll@gmail.com>
2024-05-07 18:27:24 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './jsx/App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
